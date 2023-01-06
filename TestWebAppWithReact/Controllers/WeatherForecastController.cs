ee1865e97e2ed0cad572048ec59e7b9fde41eee7
Vladyslav Lytyvnenko <Vladyslav.lytvynenko22@gmail.com>
2023-01-06 21:13:36 +0200

    added error catch

--- TestWebAppWithReact/Controllers/WeatherForecastController.cs
+++ TestWebAppWithReact/Controllers/WeatherForecastController.cs
@@ -22,14 +22,23 @@ namespace TestWebAppWithReact.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var pdf = new ChromePdfRenderer();
-            var doc = pdf.RenderHtmlAsPdf("<h1>This is a heading</h1>");
+            string? ironError = null;
+            try
+            {
+                var pdf = new ChromePdfRenderer();
+                var doc = pdf.RenderHtmlAsPdf("<h1>This is a heading</h1>");
+            }
+            catch (Exception e)
+            {
+                ironError = e.ToString();
+            }
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                IronError = ironError
             })
             .ToArray();
         }
